dcad6db417c598a56f186fb66625be4cd86c1428
Tony Pham <thanh01.pmt@gmail.com>
2024-06-16 13:39:21 +0700

    add a message label and a click button

--- Light App/ViewController.swift
+++ Light App/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblMessage: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showMessage(_ sender: Any) {
+        lblMessage.text = "Hello, World!"
+    }
+    
 }
 
